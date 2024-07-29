2e4685cacd7c0afd799f26b47c4e9d828296eb6a
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-07-29 16:15:50 +0600

    default light theme

--- src/lib/Providers/NextUiProvider.tsx
+++ src/lib/Providers/NextUiProvider.tsx
@@ -8,7 +8,7 @@ import {ThemeProvider as NextThemesProvider} from "next-themes";
 export function NextUiProvider({children}: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-       <NextThemesProvider attribute="class" defaultTheme="dark"> 
+       <NextThemesProvider attribute="class" defaultTheme="light" > 
          {children}
        </NextThemesProvider>
     </NextUIProvider>
