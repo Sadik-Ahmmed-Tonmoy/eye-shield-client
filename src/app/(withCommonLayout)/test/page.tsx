4335dd23a48dd71e4799e90ad7e0f2a4d887a1a8
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-08-14 01:51:34 +0600

    horizontal page change

---
+++ src/app/(withCommonLayout)/test/page.tsx
@@ -0,0 +1,16 @@
+
+import HorizontalScroll from '@/components/ui/HorizontalScroll';
+import SmoothScrollVertical from '@/components/ui/SmoothScrollVertical';
+import React from 'react';
+
+const TestPage = () => {
+    return (
+        <div>
+       
+            {/* <SmoothScrollVertical /> */}
+            <HorizontalScroll />
+        </div>
+    );
+};
+
+export default TestPage;
