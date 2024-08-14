54773d46261c891e0a19ec836eba2b177c94de94
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-08-15 00:09:05 +0600

    horizontal scroll

--- src/components/ui/HorizontalScroll.tsx
+++ src/components/ui/HorizontalScroll.tsx
@@ -1,14 +1,12 @@
 // @ts-nocheck
 'use client'
-import Image from 'next/image'
-
 import { useEffect, useRef } from 'react'
 import { animate, scroll, spring } from 'motion'
 import { ReactLenis } from '@studio-freight/react-lenis'
-
+import './HorizontalScroll.css'
 
 export default function HorizontalScroll(): JSX.Element {
-  const ulRef = useRef<HTMLUListElement | null>()
+  const ulRef = useRef<HTMLUListElement | null>(null)
 
   useEffect(() => {
     const items = document.querySelectorAll('li')
@@ -17,103 +15,76 @@ export default function HorizontalScroll(): JSX.Element {
       const controls = animate(
         ulRef.current,
         {
+          // transform: ['none', `translateX(-${(items.length - 1) * 62.50}vw) `],
           transform: ['none', `translateX(-${items.length - 1}00vw)`],
         },
         { easing: spring() }
       )
-      scroll(controls, { target: document.querySelector('section') })
+      scroll(controls, { target: document.getElementById('trigger') })
     }
-
-    const segmentLength = 1 / items.length
-    items.forEach((item, i) => {
-      const header = item.querySelector('h2')
-
-      scroll(animate([header], { x: [800, -800] }), {
-        target: document.querySelector('section'),
-        offset: [
-          [i * segmentLength, 1],
-          [(i + 1) * segmentLength, 0],
-        ],
-      })
-    })
   }, [])
 
   return (
     <ReactLenis root>
       <main>
         <article>
-          <header className="text-white relative  w-full bg-slate-950  grid place-content-center  h-[80vh]">
+          <div className="text-white relative  w-full bg-slate-950 grid place-content-center h-[80vh] slim-scroll2">
             <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
 
             <h1 className="text-6xl font-bold text-center tracking-tight">
               I know You Love to Scroll <br />
               So Scroll
             </h1>
-          </header>
-          <section className="h-[500vh] relative">
-            <ul ref={ulRef} className="flex sticky top-0">
-              <li className="h-screen w-screen bg-red-400 flex flex-col justify-center overflow-hidden  items-center">
-                <h2 className="text-[20vw] font-semibold relative bottom-5 inline-block text-black">
-                  PASSION
-                </h2>
-                <Image
-                  src="https://res.cloudinary.com/dzl9yxixg/image/upload/v1713532202/ui-layout/team_gsu8ej.png"
-                  className="2xl:w-[550px] w-[380px] absolute bottom-0"
-                  width={500}
-                  height={500}
-                  alt="image"
-                />
+          </div>
+          {/* <section ref={ulRef} className="h-[300vh] relative slim-scroll2">
+            <ul  className="flex justify-between sticky top-0  slim-scroll2">
+              <li className="h-screen w-screen flex-shrink-0 bg-red-400">
+                <h1>Item 1</h1>
               </li>
-              <li className="h-screen w-screen bg-blue-400 flex flex-col justify-center overflow-hidden  items-center">
-                <h2 className="text-[20vw] font-semibold relative bottom-5 inline-block text-black">
-                  WORK
-                </h2>
-                <Image
-                  src="https://res.cloudinary.com/dzl9yxixg/image/upload/v1713532202/ui-layout/team_gsu8ej.png"
-                  className="2xl:w-[550px] w-[380px] absolute bottom-0"
-                  width={500}
-                  height={500}
-                  alt="image"
-                />
+              <li className="h-screen w-screen flex-shrink-0 bg-green-400">
+                <h1>Item 2</h1>
               </li>
-              <li className="h-screen w-screen bg-orange-400 flex flex-col justify-center overflow-hidden  items-center">
-                <h2 className="text-[20vw] font-semibold relative bottom-5 inline-block text-black">
-                  MOTIVATION
-                </h2>
-                <Image
-                  src="https://res.cloudinary.com/dzl9yxixg/image/upload/v1713532202/ui-layout/team_gsu8ej.png"
-                  className="2xl:w-[550px] w-[380px] absolute bottom-0"
-                  width={500}
-                  height={500}
-                  alt="image"
-                />
+              <li className="h-screen w-screen flex-shrink-0 bg-yellow-400">
+                <h1>Item 3</h1>
               </li>
-              <li className="h-screen w-screen bg-yellow-400 flex flex-col justify-center overflow-hidden  items-center">
-                <h2 className="text-[20vw] font-semibold relative bottom-5 inline-block text-black">
-                  INSPIRATION
-                </h2>
-                <Image
-                  src="https://res.cloudinary.com/dzl9yxixg/image/upload/v1713532202/ui-layout/team_gsu8ej.png"
-                  className="2xl:w-[550px] w-[380px] absolute bottom-0"
-                  width={500}
-                  height={500}
-                  alt="image"
-                />
+              <li className="h-screen w-screen flex-shrink-0 bg-red-400">
+                <h1>Item 4</h1>
               </li>
-              <li className="h-screen w-screen bg-green-400 flex flex-col justify-center overflow-hidden  items-center">
-                <h2 className="text-[20vw] font-semibold relative bottom-5 inline-block text-black">
-                  BELIVE
-                </h2>
-                <Image
-                  src="https://res.cloudinary.com/dzl9yxixg/image/upload/v1713532202/ui-layout/team_gsu8ej.png"
-                  className="2xl:w-[550px] w-[380px] absolute bottom-0"
-                  width={500}
-                  height={500}
-                  alt="image"
-                />
+              <li className="h-screen w-screen flex-shrink-0 bg-green-400">
+                <h1>Item 5</h1>
               </li>
+              <li className="h-screen w-screen flex-shrink-0 bg-blue-400">
+                <h1>Item 6</h1>
+              </li>
+              
             </ul>
-          </section>
+          </section> */}
+
+          <div id='trigger' className="h-[200vh] relative ">
+            <section className="sticky top-0">
+              {/* <div className='h-20 w-20 bg-green-500'>2</div> */}
+              <ul ref={ulRef}  className="   flex justify-between overflow-x-aut slim-scroll2">
+                <li className="h-screen w-screen flex-shrink-0 bg-red-400">
+                  <h1>Item 1</h1>
+                </li>
+                <li className="h-screen w-screen flex-shrink-0 bg-green-400">
+                  <h1>Item 2</h1>
+                </li>
+                <li className="h-screen w-screen flex-shrink-0 bg-yellow-400">
+                  <h1>Item 3</h1>
+                </li>
+                <li className="h-screen w-screen flex-shrink-0 bg-red-400">
+                  <h1>Item 4</h1>
+                </li>
+                <li className="h-screen w-screen flex-shrink-0 bg-green-400">
+                  <h1>Item 5</h1>
+                </li>
+                <li className="h-screen w-screen flex-shrink-0 bg-blue-400">
+                  <h1>Item 6</h1>
+                </li>
+              </ul>
+            </section>
+          </div>
           <footer className="bg-red-600 text-white grid place-content-center h-[80vh]">
             <p>
               Created By{' '}
@@ -123,7 +94,7 @@ export default function HorizontalScroll(): JSX.Element {
             </p>
           </footer>
         </article>
-        <div className="progress fixed left-0 right-0  h-2 rounded-full bg-red-600 bottom-[50px] scale-x-0"></div>
+        <div className="progress fixed left-0 right-0 h-2 rounded-full bg-red-600 bottom-[50px] scale-x-0"></div>
       </main>
     </ReactLenis>
   )
